0843b4d7cfec5c35211ea4dc3a34e153e49e0390
shjhhc <1047367065@qq.com>
2016-10-30 16:57:31 +0800

    one

---
+++ src/main/java/shjh/system/web/config/WebInitializer.java
@@ -0,0 +1,13 @@
+package shjh.system.web.config;
+
+import org.springframework.core.annotation.Order;
+
+/**
+ * 所有实现了WebApplicationInitializer接口的类都会在容器启动时自动被加载运行，用@Order注解设定加载顺序
+ * 这是servlet3.0+后加入的特性，web.xml中可以不需要配置内容，都硬编码到WebApplicationInitializer的实现类中
+ * Created by shjh on 2016/10/30.
+ */
+@Order
+/*spring DispatcherServlet的配置,其它servlet和监听器等需要额外声明，用@Order注解设定启动顺序*/
+public class WebInitializer {
+}
