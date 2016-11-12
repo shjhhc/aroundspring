5571cfc1046043b29003670b2555c0aacef12eba
shjhhc <1047367065@qq.com>
2016-11-12 23:18:54 +0800

    11

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aroundspring
 构建一个围绕的spingmvc+mybatis+spring data的基础项目,以后可用作扩展。
+
+全部采用java config配置，剔除xml配置文件
---
+++ src/main/java/shjh/system/web/servlet/MyHttpServlet.java
@@ -0,0 +1,9 @@
+package shjh.system.web.servlet;
+
+import org.springframework.web.servlet.HttpServletBean;
+
+/**
+ * Created by shjh on 2016/11/12.
+ */
+public class MyHttpServlet extends HttpServletBean {
+}
